96807390b212252c51742e044d74fe20534ed759
Imrane Chaibi <imchaibi@e2r4p9.1337.ma>
2025-03-16 21:59:33 +0000

    even more more norming

--- free.c
+++ free.c
@@ -6,27 +6,26 @@
 /*   By: imchaibi <imchaibi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 00:34:25 by imchaibi          #+#    #+#             */
-/*   Updated: 2025/03/16 21:49:00 by imchaibi         ###   ########.fr       */
+/*   Updated: 2025/03/16 21:59:17 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
 
 int	mlx_quit(t_long *data)
 {
 	// mlx_hunter(data);
 	// ft_putstr_fd("\n exiting... \n", 1);
 	// cleanup(data);
-    exit(0);
+	exit(0);
 	return (0);
 }
 
 void	handle_exit(int keycode, t_long *lng)
 {
-    if (keycode == 65307)
-    {
-        // free
-        exit(0);
-    }
+	if (keycode == 65307)
+	{
+		// free
+		exit(0);
+	}
 }
