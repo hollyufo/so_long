2c5d8e87e5bda24d37b5e6e75ba4d5c3aa62328f
Imrane Chaibi <imchaibi@e2r1p8.1337.ma>
2025-03-30 17:55:58 +0000

    done with norming so_long.h

--- free.c
+++ free.c
@@ -6,20 +6,11 @@
 /*   By: imchaibi <imchaibi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 00:34:25 by imchaibi          #+#    #+#             */
-/*   Updated: 2025/03/29 23:28:54 by imchaibi         ###   ########.fr       */
+/*   Updated: 2025/03/30 00:10:33 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-int	mlx_quit(t_long *data)
-{
-	// mlx_hunter(data);
-	// ft_putstr_fd("\n exiting... \n", 1);
-	// cleanup(data);
-	exit(0);
-	return (0);
-}
 
 void	handle_exit(int keycode, t_long *lng)
 {
--- so_long.h
+++ so_long.h
@@ -6,18 +6,18 @@
 /*   By: imchaibi <imchaibi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:18:22 by imchaibi          #+#    #+#             */
-/*   Updated: 2025/03/29 23:33:40 by imchaibi         ###   ########.fr       */
+/*   Updated: 2025/03/30 17:55:19 by imchaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG
-#define SO_LONG
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
 # include <X11/X.h>
-#include <mlx.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include <mlx.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <fcntl.h>
 # include <stdarg.h>
 # include <stddef.h>
@@ -25,78 +25,78 @@
 # include <limits.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
-# endif 
+# endif
+
 typedef struct s_img
 {
-    void	*img;
-    int		width;
-    int		height;
-}   t_img;
+	void	*img;
+	int		width;
+	int		height;
+}	t_img;
 
 typedef struct s_long
 {
-    void    *mlx;
-    void    *img;
-    void    *win;
-    char    **map;
-    int     map_len;
-    int     map_width;
-    int     player_x;
-    int     player_y;
-    int     collectible_count;
-    int     exit_found;
-    t_img    *wall;
-    t_img    *player;
-    t_img    *collectible;
-    t_img    *exit;
-    t_img    *floor;
-}               t_long;
+	void	*mlx;
+	void	*img;
+	void	*win;
+	char	**map;
+	int		map_len;
+	int		map_width;
+	int		player_x;
+	int		player_y;
+	int		collectible_count;
+	int		exit_found;
+	t_img	*wall;
+	t_img	*player;
+	t_img	*collectible;
+	t_img	*exit;
+	t_img	*floor;
+}		t_long;
 
 typedef struct s_fill_data
 {
-    int     collected;
-    int     exit_found;
-    int     rows;
-    int     cols;
-}               t_fill_data;
+	int	collected;
+	int	exit_found;
+	int	rows;
+	int	cols;
+}		t_fill_data;
 
-char        *get_next_line(int fd);
-size_t      ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char        *ft_strjoin(char const *s1, char const *s2);
-size_t      ft_strlen(const char *str);
-void        *ft_memset(void *s, int c, size_t n);
-void        *ft_calloc(size_t count, size_t size);
-char        *ft_strchr(const char *s, int c);
-char        *ft_strcpy(char *dest, const char *src);
-ssize_t     gnl_buff(char *buffer, char *remainder, int fd);
-int         ft_printf(const char *format, ...);
-void        ft_putchar_fd(char c, int fd, int *count);
-void        ft_putstr_fd(char *s, int fd, int *count);
-void        ft_putnbr_fd(int n, int fd, int *count);
-void        ft_putnbr_unsigned_fd(unsigned int n, int fd, int *count);
-void        ft_putptr_fd(void *ptr, int fd, int *count);
-void        ft_puthex(unsigned long num, int fd, char case_type, int *count);
-int         ft_strncmp(const char *s1, const char *s2, size_t n);
-int         check_extension(char *filename);
-int         validate_map_elements(t_long *lng);
-int         validate_map_boundaries(t_long *lng);
-int         validate_map_structure(t_long *lng);
-int         initialise_validate_map(int ac, char *str, t_long *lng);
-void        get_map(t_long *lng, int fd);
-void        map_panic_exit(t_long *lng, const char *error_msg);
-void        map_dimensions(t_long *lng, int fd);
-char        *ft_strdup(const char *src);
-int         is_valid_path(t_long *lng);
-int         handle_keyboard(int keycode, t_long *lng);
-int         load_textures(t_long *lng);
-void        render_tile(t_long *lng, int x, int y);
-void        render_map(t_long *lng);
-int         mlx_quit(t_long *data);
-void        handle_exit(int keycode, t_long *lng);
-void        exit_game(t_long *lng);
-void        free_images(void *mlx, t_img *img);
-void        free_map(char **map);
-void        cleanup(t_long *game);
-
+char		*get_next_line(int fd);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char		*ft_strjoin(char const *s1, char const *s2);
+size_t		ft_strlen(const char *str);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_calloc(size_t count, size_t size);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strcpy(char *dest, const char *src);
+ssize_t		gnl_buff(char *buffer, char *remainder, int fd);
+int			ft_printf(const char *format, ...);
+void		ft_putchar_fd(char c, int fd, int *count);
+void		ft_putstr_fd(char *s, int fd, int *count);
+void		ft_putnbr_fd(int n, int fd, int *count);
+void		ft_putnbr_unsigned_fd(unsigned int n, int fd, int *count);
+void		ft_putptr_fd(void *ptr, int fd, int *count);
+void		ft_puthex(unsigned long num, int fd, char case_type, int *count);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			check_extension(char *filename);
+int			validate_map_elements(t_long *lng);
+int			validate_map_boundaries(t_long *lng);
+int			validate_map_structure(t_long *lng);
+int			initialise_validate_map(int ac, char *str, t_long *lng);
+void		get_map(t_long *lng, int fd);
+void		map_panic_exit(t_long *lng, const char *error_msg);
+void		map_dimensions(t_long *lng, int fd);
+char		*ft_strdup(const char *src);
+int			mlx_quit(t_long *data);
+int			is_valid_path(t_long *lng);
+int			handle_keyboard(int keycode, t_long *lng);
+int			load_textures(t_long *lng);
+void		render_tile(t_long *lng, int x, int y);
+void		render_map(t_long *lng);
+void		handle_exit(int keycode, t_long *lng);
+void		exit_game(t_long *lng);
+void		free_images(void *mlx, t_img *img);
+void		free_map(char **map);
+void		cleanup(t_long *game);
 
 #endif
